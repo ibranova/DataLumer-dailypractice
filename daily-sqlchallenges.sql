4037ed4c2bbd6ed941ac71eb6d0288a0c08ac033
Marcy Student <ibsory002@gmail.com>
2026-05-12 11:23:45 -0400

    Day two of sql practice done

--- daily-sqlchallenges.sql
+++ daily-sqlchallenges.sql
@@ -12,3 +12,17 @@ FROM
     where tweet_date Between '01/01/2022'  and '12/31/2022'
     group by user_id) as a 
 group by tweet_bucket order by 1 ;
+
+-- Day 2: Data Science Skills
+/*Given a table of candidates and their skills, you're tasked with finding the candidates best suited for an open Data Science job.
+You want to find candidates who are proficient in Python, Tableau, and PostgreSQL.
+
+Write a query to list the candidates who possess all of the required skills for the job.
+ Sort the output by candidate ID in ascending order.*/
+
+ -- SQL Query: 
+ SELECT candidate_id
+FROM candidates
+where skill in ('Python', 'Tableau', 'PostgreSQL')
+GROUP BY candidate_id
+HAVING count(skill) = 3;
